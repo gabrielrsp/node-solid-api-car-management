7f4f645fb11a4552e7eca9578453cb41130952f3
gabrielrsp <gabrielrsp18@gmail.com>
2022-01-17 23:28:58 -0300

    add create rental use case

--- src/shared/infra/typeorm/migrations/1625406415185-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1625406415185-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1625406415185 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
