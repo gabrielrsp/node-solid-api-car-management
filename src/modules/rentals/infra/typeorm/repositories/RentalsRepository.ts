4786806f8f7691e457eb5819005d9d57ffad6d5b
gabriel <gabrielrsp18@gmail.com>
2022-02-15 23:25:31 -0300

    update swagger and join cars on finding rentals by user

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -26,6 +26,7 @@ class RentalsRepository implements IRentalsRepository {
   async findByUser (user_id: string): Promise<Rental[]> {
     const rentals = await this.repository.find({
       where: { user_id },
+      relations: ["car"],
     });
     return rentals;
   }
