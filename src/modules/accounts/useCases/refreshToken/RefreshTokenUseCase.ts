8ed39658809ec719b3f683aa2f4ee8f22260be3a
gabriel <gabrielrsp18@gmail.com>
2022-02-24 21:41:57 -0300

    create refresh token controller

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -1,30 +1,28 @@
 import auth from "@config/auth";
 import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
 import { verify, sign } from "jsonwebtoken";
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
-import { AppError } from "@shared/errors/AppError";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
   sub: string;
   email: string;
 }
 
+@injectable()
 class RefreshTokenUseCase {
   constructor(
     @inject("UsersTokensRepository")
-    private usersTokensRepository: IUsersTokensRepository
+    private usersTokensRepository: IUsersTokensRepository,
 
     @inject("DayjsDateProvider")
     private dateProvider: IDateProvider
   ) { }
 
   async execute (token: string): Promise<string> {
-    const { email, sub } = verify(
-      token,
-      auth.secret_token
-    ) as IPayload;
+    const { email, sub } = verify(token, auth.secret_refresh_token) as IPayload;
 
     const user_id = sub;
 
@@ -40,10 +38,9 @@ class RefreshTokenUseCase {
 
     await this.usersTokensRepository.deleteById(userToken.id);
 
-
     const refresh_token = sign({ email }, auth.secret_refresh_token, {
       subject: sub,
-      expiresIn: auth.expires_refresh_token_days
+      expiresIn: auth.expires_refresh_token_days,
     });
 
     const expires_date = this.dateProvider.addDays(
@@ -53,10 +50,10 @@ class RefreshTokenUseCase {
     await this.usersTokensRepository.create({
       expires_date,
       refresh_token,
-      user_id
-    })
+      user_id,
+    });
 
-    return refresh_token
+    return refresh_token;
   }
 }
 
--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,4 +1,6 @@
+import auth from "@config/auth";
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 
@@ -15,6 +17,8 @@ export async function ensureAuthenticated (
 ) {
   const authHeader = request.headers.authorization;
 
+  const usersTokensRepository = new UsersTokensRepository();
+
   if (!authHeader) {
     throw new AppError("missing Token", 401);
   }
@@ -24,11 +28,13 @@ export async function ensureAuthenticated (
   try {
     const { sub: user_id } = verify(
       token,
-      "d652eeeea9a382e2b37ad73e0a66b131"
+      auth.secret_refresh_token
     ) as IPayload;
 
-    const usersRepository = new UsersRepository();
-    const user = usersRepository.findById(user_id);
+    const user = usersTokensRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token
+    );
 
     if (!user) {
       throw new AppError("User does not exists", 401);
