aa2fb8de3dd1291242b20ced2808ba4259f5c2fd
gabriel <gabrielrsp18@gmail.com>
2022-03-13 19:29:50 -0300

    reset password by sending email with token

--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -6,32 +6,36 @@ import { IDateProvider } from "../IDateProvider";
 dayjs.extend(utc);
 
 class DayjsDateProvider implements IDateProvider {
-  compareInHours (start_date: Date, end_date: Date): number {
+  compareInHours(start_date: Date, end_date: Date): number {
     const end_date_utc = this.convertToUTC(end_date);
     const start_date_utc = this.convertToUTC(start_date);
     return dayjs(end_date_utc).diff(start_date_utc, "hours");
   }
 
-  convertToUTC (date: Date): string {
+  convertToUTC(date: Date): string {
     return dayjs(date).utc().local().format();
   }
 
-  dateNow (): Date {
+  dateNow(): Date {
     return dayjs().toDate();
   }
 
-  compareInDays (start_date: Date, end_date: any): number {
+  compareInDays(start_date: Date, end_date: any): number {
     const end_date_utc = this.convertToUTC(end_date);
     const start_date_utc = this.convertToUTC(start_date);
     return dayjs(end_date_utc).diff(start_date_utc, "days");
   }
 
-  addDays (days: number): Date {
+  addDays(days: number): Date {
     return dayjs().add(days, "days").toDate();
   }
 
-  addHours (hours: number): Date {
+  addHours(hours: number): Date {
     return dayjs().add(hours, "hour").toDate();
+  }
+
+  compareIfBefore(start_date: Date, end_date: Date): boolean {
+    return dayjs(start_date).isBefore(end_date);
   }
 }
 
