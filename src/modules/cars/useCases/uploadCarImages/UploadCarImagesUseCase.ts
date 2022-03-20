b70f27db55defc98b1a024819897b762719e3b2f
gabrielrsp <gabrielrsp18@gmail.com>
2022-03-19 23:02:04 -0300

    create get profile route

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,6 +1,8 @@
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
 import { inject, injectable } from "tsyringe";
 
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
+
 interface IRequest {
   car_id: string;
   images_name: string[];
@@ -10,11 +12,14 @@ interface IRequest {
 class UploadCarsImagesUseCase {
   constructor(
     @inject("CarsImagesRepository")
-    private carsImagesRepository: ICarsImagesRepository
-  ) { }
-  async execute ({ car_id, images_name }: IRequest): Promise<void> {
+    private carsImagesRepository: ICarsImagesRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
+  ) {}
+  async execute({ car_id, images_name }: IRequest): Promise<void> {
     images_name.map(async (image) => {
       await this.carsImagesRepository.create(car_id, image);
+      await this.storageProvider.save(image, "cars");
     });
   }
 }
--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import uploadConfig from "@config/upload";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUser/ProfileUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 import { Router } from "express";
 import multer from "multer";
@@ -14,6 +15,8 @@ const createUserController = new CreateUserController();
 
 const updateUserAvatarController = new UpdateUserAvatarController();
 
+const profileUserController = new ProfileUserController();
+
 usersRoutes.post("/", createUserController.handle);
 
 usersRoutes.patch(
@@ -22,5 +25,7 @@ usersRoutes.patch(
   uploadAvatar.single("avatar"),
   updateUserAvatarController.handle
 );
+
+usersRoutes.get("/profile", ensureAuthenticated, profileUserController.handle);
 
 export { usersRoutes };
